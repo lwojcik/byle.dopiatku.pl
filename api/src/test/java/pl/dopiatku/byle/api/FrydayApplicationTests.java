4f3ad3e3e8977fb642dce0208f0ae20a4fb1060e
Andrzej  Matuszewski <50904391+akmghub@users.noreply.github.com>
2020-02-21 07:50:43 +0100

    Feature/api (#2)
    * Dodanie javovego API zwracającego zestaw json'ów opisujących piątki
    
    * dodanie README.md
    
    Co-authored-by: andrzejmatuszewski <32356775+andrzejmatuszewski@users.noreply.github.com>

---
+++ api/src/test/java/pl/dopiatku/byle/api/FrydayApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dopiatku.byle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
